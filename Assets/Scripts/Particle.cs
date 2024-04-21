eeeebf7d378a65fe9105e79185c3efcda3568018
TeeNik <teenik3315@gmail.com>
2024-04-22 01:09:21 +0200

    particle prefab

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-    public float Gravity;
-    public Vector2 BoundsHalfSize;
+    public float Gravity = 9.8f;
+    public float Radius = 25.0f; 
 
     private Vector2 _position;
     private Vector2 _velocity;
@@ -19,17 +19,32 @@ public class Particle : MonoBehaviour
         _position = _rect.localPosition;
     }
 
+    private void OnValidate()
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Radius * 2, Radius * 2);
+    }
+
     void Update()
     {
         _velocity += Gravity * Time.deltaTime * Vector2.down;
         _position += _velocity * Time.deltaTime;
+        ResolveCollisions();
 
         _rect.localPosition = _position;
     }
 
-    private void OnDrawGizmos()
+    private void ResolveCollisions()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(Vector2.zero, BoundsHalfSize * 2);
+        Vector2 halfScreenSize = new Vector2(Screen.width, Screen.height) / 2;
+        if (Mathf.Abs(_position.x) > halfScreenSize.x)
+        {
+            _position.x = halfScreenSize.x * Mathf.Sign(_position.x);
+            _velocity.x *= -1;
+        }
+        if (Mathf.Abs(_position.y) > halfScreenSize.y)
+        {
+            _position.y = halfScreenSize.y * Mathf.Sign(_position.y);
+            _velocity.y *= -1;
+        }
     }
 }
