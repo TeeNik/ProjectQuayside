4514e9655a27e06bf47f03aefcfe9d21f356e5fd
TeeNik <teenik3315@gmail.com>
2025-02-06 23:13:53 +0100

    use premake to create project files

---
+++ premake5.lua
@@ -0,0 +1,115 @@
+workspace "Quayside"
+	architecture "x64"
+
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Quayside"
+	location "Quayside"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	buildoptions
+	{
+		"/utf-8"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"QS_PLATFORM_WINDOWS",
+			"QS_BUILD_DLL"
+		}
+	
+		postbuildcommands
+		{
+			("copy /B /Y ..\\bin\\" .. outputdir .. "\\%{prj.name}\\%{prj.name}.dll ..\\bin\\" .. outputdir .. "\\Sandbox\\ > nul")
+		}
+
+	filter "configurations:Debug"
+		defines "QS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "QS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "QS_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Quayside/vendor/spdlog/include",
+		"Quayside/src"
+	}
+
+	links
+	{
+		"Quayside"
+	}
+
+	buildoptions
+	{
+		"/utf-8"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"QS_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "QS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "QS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "QS_DIST"
+		optimize "On"
