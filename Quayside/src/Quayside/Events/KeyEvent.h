7e17abe2c90ab8df1f3c3e3917db107621c0b310
TeeNik <teenik3315@gmail.com>
2025-02-08 16:30:51 +0100

    key event class

---
+++ Quayside/src/Quayside/Events/KeyEvent.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "Event.h"
+#include <sstream>
+
+namespace Quayside
+{
+    class QUAYSIDE_API KeyEvent : public Event
+    {
+    public:
+        virtual ~KeyEvent() = default;
+        inline int GetKeyCode() const {return KeyCode;}
+
+        EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << GetName() << ": " << GetKeyCode();
+            return ss.str();
+        }
+        
+    protected:
+        KeyEvent(int InKeyCode) : KeyCode(InKeyCode) {}
+        
+        int KeyCode;
+    };
+
+    class QUAYSIDE_API KeyPressedEvent : public KeyEvent
+    {
+    public:
+        KeyPressedEvent(int InKeyCode) : KeyEvent(InKeyCode) {}
+        EVENT_CLASS_TYPE(KeyPressed)
+    };
+
+    class QUAYSIDE_API KeyHoldEvent : public KeyEvent
+    {
+    public:
+        KeyHoldEvent(int InKeyCode) : KeyEvent(InKeyCode) {}
+        EVENT_CLASS_TYPE(KeyHold)
+    };
+
+    class QUAYSIDE_API KeyReleasedEvent : public KeyEvent
+    {
+    public:
+        KeyReleasedEvent(int InKeyCode) : KeyEvent(InKeyCode) {}
+        EVENT_CLASS_TYPE(KeyReleased)
+    };
+}
