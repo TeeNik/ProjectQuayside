fb869974406bf17650c66d349b09a31c92181a38
TeeNik <teenik3315@gmail.com>
2025-02-10 15:57:37 +0100

    application events

---
+++ Quayside/src/Quayside/Events/ApplicationEvent.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <sstream>
+#include "Event.h"
+
+namespace Quayside
+{
+    class QUAYSIDE_API WindowResizeEvent : Event
+    {
+    public:
+        WindowResizeEvent(int InWidth, int InHeight) : Width(InWidth), Height(InHeight) {}
+
+        inline unsigned int GetWidth() const { return Width; }
+        inline unsigned int GetHeight() const { return Height; }
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "WindowResizeEvent: " << Width << ", " << Height;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(WindowResize)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+    private:
+        unsigned int Width, Height;
+    };
+    
+    class QUAYSIDE_API WindowCloseEvent : Event
+    {
+    public:
+        EVENT_CLASS_TYPE(WindowClose)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    };
+
+    class QUAYSIDE_API AppTickEvent : Event
+    {
+    public:
+        EVENT_CLASS_TYPE(AppTick)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    };
+
+    class QUAYSIDE_API AppRenderEvent : Event
+    {
+    public:
+        EVENT_CLASS_TYPE(AppRender)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    };
+}
--- Quayside/src/Quayside/Events/Event.h
+++ Quayside/src/Quayside/Events/Event.h
@@ -38,6 +38,7 @@ namespace Quayside
 	{
 		friend class EventDispatcher;
 	public:
+		virtual ~Event() = default;
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
