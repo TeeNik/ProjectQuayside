7df73ab7bec8f38cc0a84df2b328a31ec83febab
TeeNik <teenik3315@gmail.com>
2025-02-25 02:05:09 +0100

    add Key & Mouse codes

--- Quayside/src/Quayside.h
+++ Quayside/src/Quayside.h
@@ -5,6 +5,10 @@
 #include "Quayside/Application.h"
 #include "Quayside/Log.h"
 #include "Quayside/Layer.h"
+#include "Quayside/Input.h"
+#include "Quayside/MouseButtonCodes.h"
+#include "Quayside/KeyCodes.h"
+
 #include "Quayside/ImGui/ImGuiLayer.h"
 
 
